b796f82f6268c52de638a39bcdfd52e2b7385369
6sRyuSK <zikkyou19@gmail.com>
2020-05-26 09:00:29 +0900

    player.vueの追加とheader content playerのheight比率を調整

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="w-screen h-screen">
-    <nav id="app-header" class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <nav id="app-header" class="flex items-center justify-between flex-wrap bg-teal-500">
       <div class="flex items-center flex-shrink-0 text-white mr-6">
         <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
         <span class="font-semibold text-xl tracking-tight">Onsen Spot</span>
@@ -10,16 +10,20 @@
       <div id="side-bar" class="bg-teal-600"></div>
       <program-list v-if="showProgramList"></program-list>
     </div>
+    <player />
   </div>
 </template>
 
 <script>
 import { ALL_PROGRAM } from './constants/program'
 import programList from './components/programList'
+import player from './components/player'
+
 export default {
   name: 'App',
   components: {
-    programList
+    programList,
+    player
   },
   data () {
     return {
@@ -48,11 +52,13 @@ export default {
 #app-header {
   position: sticky;
   top: 0;
+  height: 11%;
+  padding: 0 1.5rem;
 }
 #app-content {
   display: flex;
   flex-direction: row;
-  height: calc(100% - 80px);
+  height: 80%;
   #side-bar {
     display: block;
     width: 30%;
---
+++ src/components/player.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="player">
+    <h1>{{ playing }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'player',
+  computed: {
+    playing () { return this.$store.getters.getPlaying }
+  }
+}
+</script>
+
+<style lang="scss">
+#player {
+  width: 100%;
+  height: 9%;
+  background-color: black;
+}
+</style>
