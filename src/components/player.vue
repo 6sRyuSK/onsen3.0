a9b1cea10c20ba5dfc7dd9405b3e99fcee210eb9
6sRyuSK <zikkyou19@gmail.com>
2020-05-26 11:27:59 +0900

    再生中の番組のサムネ、タイトル、出演者を表示

--- src/components/player.vue
+++ src/components/player.vue
@@ -2,9 +2,19 @@
   <div id="player">
     <audio :src="playing.moviePath" id="audio-elm"></audio>
     <div class="flex h-full justify-center">
-      <div id="controll-panel h-full">
+      <div id="playing-content" class="w-4/12 flex">
+        <div class="h-full w-4/12 flex item-center">
+          <img :src="playing.thumbnailPath" class="thumbnail h-full w-auto p-1 mr-0">
+        </div>
+        <div class="w-8/12">
+          <p><marquee behavior="scroll" direction="left"><b class="text-white">{{ playing.title }}</b></marquee></p>
+          <small class="text-white">{{ playing.personality }}</small>
+        </div>
+      </div>
+      <div id="controll-panel " class="w-4/12 flex justify-center">
         <div class="play-button flex items-center select-none" @click="clickPlay"><i class="material-icons play-icon">{{ isPlaying ? 'play_arrow' : 'pause'}}</i></div>
       </div>
+      <div id="volume-controll" class="w-4/12"></div>
     </div>
   </div>
 </template>
@@ -13,7 +23,7 @@
 export default {
   name: 'player',
   computed: {
-    playing () { return this.$store.getters.getPlaying }
+    playing () { return this.$store.getters.getPlaying || { moviePath: '', thumbnailPath: '', title: '', personality: '' } }
   },
   data () {
     return {
@@ -35,6 +45,12 @@ export default {
   width: 100%;
   height: 9%;
   background-color: black;
+  .playing-content {
+    color: white;
+    .thumbnail {
+      margin: 0;
+    }
+  }
   .play-button {
     height: 100%;
     .play-icon {
