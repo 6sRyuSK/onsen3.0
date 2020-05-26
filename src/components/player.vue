62f6bc10cec34e216efc748f9584e8079660d372
6sRyuSK <zikkyou19@gmail.com>
2020-05-26 10:26:08 +0900

    再生/一時的 を追加

--- src/components/player.vue
+++ src/components/player.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="player">
-    <h1>{{ playing }}</h1>
+    <audio :src="playing.moviePath" id="audio-elm"></audio>
+    <div class="flex h-full justify-center">
+      <div id="controll-panel h-full">
+        <div class="play-button flex items-center select-none" @click="clickPlay"><i class="material-icons play-icon">{{ isPlaying ? 'play_arrow' : 'pause'}}</i></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +14,18 @@ export default {
   name: 'player',
   computed: {
     playing () { return this.$store.getters.getPlaying }
+  },
+  data () {
+    return {
+      isPlaying: false
+    }
+  },
+  methods: {
+    clickPlay () {
+      const audioElm = document.getElementById('audio-elm')
+      this.isPlaying ? audioElm.pause() : audioElm.play()
+      this.isPlaying = !this.isPlaying
+    }
   }
 }
 </script>
@@ -18,5 +35,16 @@ export default {
   width: 100%;
   height: 9%;
   background-color: black;
+  .play-button {
+    height: 100%;
+    .play-icon {
+      width: auto;
+      height: auto;
+      color: white;
+      font-size: 2.5rem;
+      border: solid white 2px;
+      border-radius: 25px;
+    }
+  }
 }
 </style>
